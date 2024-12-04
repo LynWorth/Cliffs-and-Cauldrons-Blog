dd733a6cf5b5725e8ff4db897ba15650b8771b00
Lynda Worth <lynw038@gmail.com>
2024-12-04 00:00:05 +0000

    add to base html, event html and testing event model

--- event/admin.py
+++ event/admin.py
@@ -5,17 +5,16 @@ from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
 
 
-#@admin.register(Event)
-#class EventAdmin(admin.ModelAdmin):
-#    list_display = ("title", "event_date", "created_on")  # Fields to display in the admin list view
-#    search_fields = ("title", "description")  # Fields for the search bar
-#    list_filter = ("event_date",)  # Filters in the admin sidebar
-
-
-from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Event)
 class EventAdmin(SummernoteModelAdmin):
     list_display = ("title", "event_date", "location", "recurrence", "is_public")
     list_filter = ("recurrence", "is_public", "event_date")
-    summernote_fields = ('description',)  # Specify fields to use the Summernote editor
+    summernote_fields = ('description',) # Specify fields to use the Summernote editor
+
+
+#@admin.register(Event)
+#class EventAdmin(admin.ModelAdmin):
+  #  list_display = ("title", "event_date", "created_on")  # Fields to display in the admin list view
+   # search_fields = ("title", "description")  # Fields for the search bar
+   # list_filter = ("event_date",)  # Filters in the admin sidebar
--- event/views.py
+++ event/views.py
@@ -1,32 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse
 from .models import Event
 from django.utils.timezone import now
 
-def upcoming_events(request):
-    events = Event.objects.filter(is_public=True, event_date__gte=now())
-    recurring_events = events.exclude(recurrence="none")
-    single_events = events.filter(recurrence="none")
-    return render(request, "events.html", {"events": single_events | recurring_events})
 
+def home(request):
+    # Fetch three events
+    event = Event.objects.filter(is_public=True).order_by('event_date')[:3]
+    return render(request, "base.html", {"event": event})
 
-
-# Create your views here.
 def event_detail(request, event_id):
-    
-    #event = event.objects.all().order_by('-updated_on').first()
-
-    queryset = Event.objects.filter(status=1)
-    event = get_object_or_404(queryset, event_id = event_id)
-
-
-    return render(
-        request,
-        "event/event_detail.html",
-        {"event": event},
-    )
-
-
-class EventsList(generic.Listview):
-    model = Event
-    template_name = "event/index.html"
-    paginate_by = 12
+    # Fetch a single event by ID
+    event = get_object_or_404(Event, id=event_id)
+    return render(request, "event.html", {"event": event})
